4ef3c6cd6b3e07a01bb0960b2baef81e9734f6f6
Jake Howard <git@theorangeone.net>
2026-02-06 14:54:35 +0000

    Add module init

--- django_tasks_db/__init__.py
+++ django_tasks_db/__init__.py
@@ -1,3 +1,12 @@
+# ruff: noqa: E402
+import django_stubs_ext
+
+django_stubs_ext.monkeypatch()
+
+import importlib.metadata
+
+__version__ = importlib.metadata.version(__name__)
+
 from .backend import DatabaseBackend
 
 __all__ = ["DatabaseBackend"]
