RealOrangeOne/django-tasks

Add DB indexes (#155)
Co-authored-by: Jonatron <jonath4n@gmail.com>

django_tasks/backends/database/migrations/0016_alter_dbtaskresult_options_and_more.py
@@ -1,0 +1,70 @@
+# Generated by Django 5.2 on 2025-05-02 13:48
+
+import datetime
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("django_tasks_database", "0015_correctly_order_run_after"),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name="dbtaskresult",
+            options={
+                "ordering": [
+                    models.OrderBy(models.F("priority"), descending=True),
+                    models.OrderBy(models.F("run_after")),
+                ],
+                "verbose_name": "Task Result",
+                "verbose_name_plural": "Task Results",
+            },
+        ),
+        migrations.AlterField(
+            model_name="dbtaskresult",
+            name="backend_name",
+            field=models.CharField(max_length=32, verbose_name="backend name"),
+        ),
+        migrations.AlterField(
+            model_name="dbtaskresult",
+            name="queue_name",
+            field=models.CharField(
+                default="default", max_length=32, verbose_name="queue name"
+            ),
+        ),
+        migrations.AlterField(
+            model_name="dbtaskresult",
+            name="run_after",
+            field=models.DateTimeField(
+                default=datetime.datetime(
+                    9999, 1, 1, 0, 0, tzinfo=datetime.timezone.utc
+                ),
+                verbose_name="run after",
+            ),
+            preserve_default=False,
+        ),
+        migrations.AddIndex(
+            model_name="dbtaskresult",
+            index=models.Index(
+                models.F("status"),
+                models.OrderBy(models.F("priority"), descending=True),
+                models.OrderBy(models.F("run_after")),
+                condition=models.Q(("status", "NEW")),
+                name="django_task_new_ordering_idx",
+            ),
+        ),
+        migrations.AddIndex(
+            model_name="dbtaskresult",
+            index=models.Index(
+                fields=["queue_name"], name="django_task_queue_n_99c321_idx"
+            ),
+        ),
+        migrations.AddIndex(
+            model_name="dbtaskresult",
+            index=models.Index(
+                fields=["backend_name"], name="django_task_backend_071754_idx"
+            ),
+        ),
+    ]

abnerpc/django-tasks-db

django_tasks_db/migrations/0016_alter_dbtaskresult_options_and_more.py
@@ -1,0 +1,70 @@
+# Generated by Django 5.2 on 2025-05-02 13:48
+
+import datetime
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("django_tasks_db_database", "0015_correctly_order_run_after"),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name="dbtaskresult",
+            options={
+                "ordering": [
+                    models.OrderBy(models.F("priority"), descending=True),
+                    models.OrderBy(models.F("run_after")),
+                ],
+                "verbose_name": "Task Result",
+                "verbose_name_plural": "Task Results",
+            },
+        ),
+        migrations.AlterField(
+            model_name="dbtaskresult",
+            name="backend_name",
+            field=models.CharField(max_length=32, verbose_name="backend name"),
+        ),
+        migrations.AlterField(
+            model_name="dbtaskresult",
+            name="queue_name",
+            field=models.CharField(
+                default="default", max_length=32, verbose_name="queue name"
+            ),
+        ),
+        migrations.AlterField(
+            model_name="dbtaskresult",
+            name="run_after",
+            field=models.DateTimeField(
+                default=datetime.datetime(
+                    9999, 1, 1, 0, 0, tzinfo=datetime.timezone.utc
+                ),
+                verbose_name="run after",
+            ),
+            preserve_default=False,
+        ),
+        migrations.AddIndex(
+            model_name="dbtaskresult",
+            index=models.Index(
+                models.F("status"),
+                models.OrderBy(models.F("priority"), descending=True),
+                models.OrderBy(models.F("run_after")),
+                condition=models.Q(("status", "NEW")),
+                name="django_task_new_ordering_idx",
+            ),
+        ),
+        migrations.AddIndex(
+            model_name="dbtaskresult",
+            index=models.Index(
+                fields=["queue_name"], name="django_task_queue_n_99c321_idx"
+            ),
+        ),
+        migrations.AddIndex(
+            model_name="dbtaskresult",
+            index=models.Index(
+                fields=["backend_name"], name="django_task_backend_071754_idx"
+            ),
+        ),
+    ]
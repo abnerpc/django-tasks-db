0bb8c342227689c155ba3ffb0abcb985c0b31383
Jake Howard <6527489+RealOrangeOne@users.noreply.github.com>
2025-05-02 16:21:45 +0100

    Add DB indexes (#155)
    Co-authored-by: Jonatron <jonath4n@gmail.com>

--- django_tasks_db/apps.py
+++ django_tasks_db/apps.py
@@ -5,3 +5,6 @@ class TasksAppConfig(AppConfig):
     name = "django_tasks_db"
     label = "django_tasks_db_database"
     verbose_name = "Tasks Database Backend"
+
+    def ready(self) -> None:
+        from . import signal_handlers  # noqa
