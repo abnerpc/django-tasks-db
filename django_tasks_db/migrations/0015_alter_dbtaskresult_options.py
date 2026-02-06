RealOrangeOne/django-tasks

Correctly order `run_after` tasks (#154)
With all other things equal, older tasks should come first (meaning they'll more likely to be run closer to their `run_after` date)

django_tasks/backends/database/migrations/0015_alter_dbtaskresult_options.py
@@ -1,0 +1,23 @@
+# Generated by Django 5.1.5 on 2025-04-27 12:00
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("django_tasks_database", "0014_remove_dbtaskresult_exception_data"),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name="dbtaskresult",
+            options={
+                "ordering": [
+                    models.OrderBy(models.F("priority"), descending=True),
+                    models.OrderBy(models.F("run_after"), nulls_last=True),
+                ],
+                "verbose_name": "Task Result",
+                "verbose_name_plural": "Task Results",
+            },
+        ),
+    ]

abnerpc/django-tasks-db

django_tasks_db/migrations/0015_alter_dbtaskresult_options.py
@@ -1,0 +1,23 @@
+# Generated by Django 5.1.5 on 2025-04-27 12:00
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("django_tasks_db_database", "0014_remove_dbtaskresult_exception_data"),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name="dbtaskresult",
+            options={
+                "ordering": [
+                    models.OrderBy(models.F("priority"), descending=True),
+                    models.OrderBy(models.F("run_after"), nulls_last=True),
+                ],
+                "verbose_name": "Task Result",
+                "verbose_name_plural": "Task Results",
+            },
+        ),
+    ]