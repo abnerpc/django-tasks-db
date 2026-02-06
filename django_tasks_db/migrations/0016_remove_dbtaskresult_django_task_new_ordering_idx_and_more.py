RealOrangeOne/django-tasks

Squash migrations before release

django_tasks/backends/database/migrations/0016_remove_dbtaskresult_django_task_new_ordering_idx_and_more.py
@@ -1,8 +1,24 @@
-# Generated by Django 5.2 on 2025-05-02 13:48
+# Generated by Django 5.2.2 on 2025-06-13 14:56
 
 import datetime
 
 from django.db import migrations, models
+from django.db.backends.base.schema import BaseDatabaseSchemaEditor
+from django.db.migrations.state import StateApps
+
+
+def update_status(apps: StateApps, schema_editor: BaseDatabaseSchemaEditor) -> None:
+    DBTaskResult = apps.get_model("django_tasks_database", "DBTaskResult")
+
+    DBTaskResult.objects.filter(status="NEW").update(status="READY")
+
+
+def revert_status_rename(
+    apps: StateApps, schema_editor: BaseDatabaseSchemaEditor
+) -> None:
+    DBTaskResult = apps.get_model("django_tasks_database", "DBTaskResult")
+
+    DBTaskResult.objects.filter(status="READY").update(status="NEW")
 
 
 class Migration(migrations.Migration):
@@ -45,13 +61,34 @@ class Migration(migrations.Migration):
             ),
             preserve_default=False,
         ),
+        migrations.AlterField(
+            model_name="dbtaskresult",
+            name="status",
+            field=models.CharField(
+                choices=[
+                    ("READY", "Ready"),
+                    ("RUNNING", "Running"),
+                    ("FAILED", "Failed"),
+                    ("SUCCEEDED", "Succeeded"),
+                ],
+                default="READY",
+                max_length=9,
+                verbose_name="status",
+            ),
+        ),
+        migrations.AddField(
+            model_name="dbtaskresult",
+            name="worker_ids",
+            field=models.JSONField(default=list, verbose_name="worker id"),
+        ),
+        migrations.RunPython(update_status, revert_status_rename),
         migrations.AddIndex(
             model_name="dbtaskresult",
             index=models.Index(
                 models.F("status"),
                 models.OrderBy(models.F("priority"), descending=True),
                 models.OrderBy(models.F("run_after")),
-                condition=models.Q(("status", "NEW")),
+                condition=models.Q(("status", "READY")),
                 name="django_task_new_ordering_idx",
             ),
         ),

abnerpc/django-tasks-db

django_tasks_db/migrations/0016_remove_dbtaskresult_django_task_new_ordering_idx_and_more.py
@@ -1,8 +1,24 @@
-# Generated by Django 5.2 on 2025-05-02 13:48
+# Generated by Django 5.2.2 on 2025-06-13 14:56
 
 import datetime
 
 from django.db import migrations, models
+from django.db.backends.base.schema import BaseDatabaseSchemaEditor
+from django.db.migrations.state import StateApps
+
+
+def update_status(apps: StateApps, schema_editor: BaseDatabaseSchemaEditor) -> None:
+    DBTaskResult = apps.get_model("django_tasks_db_database", "DBTaskResult")
+
+    DBTaskResult.objects.filter(status="NEW").update(status="READY")
+
+
+def revert_status_rename(
+    apps: StateApps, schema_editor: BaseDatabaseSchemaEditor
+) -> None:
+    DBTaskResult = apps.get_model("django_tasks_db_database", "DBTaskResult")
+
+    DBTaskResult.objects.filter(status="READY").update(status="NEW")
 
 
 class Migration(migrations.Migration):
@@ -45,13 +61,34 @@ class Migration(migrations.Migration):
             ),
             preserve_default=False,
         ),
+        migrations.AlterField(
+            model_name="dbtaskresult",
+            name="status",
+            field=models.CharField(
+                choices=[
+                    ("READY", "Ready"),
+                    ("RUNNING", "Running"),
+                    ("FAILED", "Failed"),
+                    ("SUCCEEDED", "Succeeded"),
+                ],
+                default="READY",
+                max_length=9,
+                verbose_name="status",
+            ),
+        ),
+        migrations.AddField(
+            model_name="dbtaskresult",
+            name="worker_ids",
+            field=models.JSONField(default=list, verbose_name="worker id"),
+        ),
+        migrations.RunPython(update_status, revert_status_rename),
         migrations.AddIndex(
             model_name="dbtaskresult",
             index=models.Index(
                 models.F("status"),
                 models.OrderBy(models.F("priority"), descending=True),
                 models.OrderBy(models.F("run_after")),
-                condition=models.Q(("status", "NEW")),
+                condition=models.Q(("status", "READY")),
                 name="django_task_new_ordering_idx",
             ),
         ),